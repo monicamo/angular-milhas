580b0e6a700dbbfe4999bf9d0476c72583695bec
Monica Monteiro <monicamo@gmail.com>
2023-11-03 14:04:37 -0300

    Instalando Service Promocao

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PromocaoService } from 'src/app/core/services/promocao.service';
+import { Promocao } from 'src/app/core/types/type';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public promocoes: Promocao[] = [];
+
   src = 'assets/imagens/banner-homepage.png';
   alt = 'Banner da home';
+
+  constructor(private servicoPromocao: PromocaoService) {
+  }
+
+  ngOnInit(): void {
+    this.servicoPromocao.listar().subscribe(
+      resposta => {
+        this.promocoes = resposta;
+        console.log(resposta);
+      }
+    );
+  }
 
 }
--- src/app/shared/card-busca/card-busca.component.ts
+++ src/app/shared/card-busca/card-busca.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Promocao } from 'src/app/core/types/type';
 
 @Component({
   selector: 'app-card-busca',
   templateUrl: './card-busca.component.html',
   styleUrls: ['./card-busca.component.scss']
 })
-export class CardBuscaComponent {
+export class CardBuscaComponent implements OnInit {
+  @Input() config!: Promocao;
+
+  constructor() {}
+  
+  ngOnInit(): void {
+    console.log(this.config)
+  }
 
 }
